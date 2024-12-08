ed6ac861109a3c6c589d7b658093f1c7bd059fd5
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-12-08 15:18:51 +0100

    Improve sample code

--- src/UnionStruct.Sample/Program.cs
+++ src/UnionStruct.Sample/Program.cs
@@ -1,25 +1,24 @@
-﻿using System.Globalization;
+﻿#pragma warning disable CA1303
+using System.Globalization;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using UnionStruct.Sample;
 using UnionStruct.Sample.Cases;
 
-unsafe
-{
-    Console.WriteLine(sizeof(TestUnion));
-}
+Console.WriteLine("\n=== Shape sample ===");
+WriteSizeOf<Shape<float>>();
+WriteShape(Shape<float>.Circle(1.0f));
+WriteShape(Shape<float>.Rectangle(2.0f, 3.0f));
 
-Shape<float> circle = Shape<float>.Circle(1.0f);
-Shape<float> rectangle = Shape<float>.Rectangle(2.0f, 3.0f);
-
-WriteShape(circle);
-WriteShape(rectangle);
-
+Console.WriteLine("\n=== TestUnion sample ===");
+WriteSizeOf<TestUnion>();
 WriteUnion(TestUnion.Empty());
 WriteUnion(TestUnion.PositionCase(new Position(new Vector3(1, 2, 3))));
 WriteUnion(TestUnion.PositionRangeCase(new PositionRange(new Vector3(1, 2, 3), new Vector3(4, 5, 6))));
 WriteUnion(TestUnion.MultiCase(new Vector3(1, 2, 3), new Vector3(4, 5, 6)));
 
+Console.WriteLine("\n=== ComplexUnion sample ===");
+WriteSizeOf<ComplexUnion<int, long>>();
 ComplexUnion<int, long> test = ComplexUnion<int, long>.UCaseNested(new ComplexUnion<int, long>.TestGenericNested<byte, short>(1, 2), 3, 4);
 Console.WriteLine(test);
 Console.WriteLine($"A: {test.UCaseNestedData.Value?.A ?? 0}");
@@ -28,11 +27,11 @@ void WriteShape<T>(Shape<T> shape)
 	where T : INumber<T>
 {
 	shape.Switch(
-		radius => Console.WriteLine($"Circle: {radius}"),
-		(width, height) => Console.WriteLine($"Rectangle: {width} {height}"));
+		radius => Console.WriteLine($"Circle: radius {radius}"),
+		(width, height) => Console.WriteLine($"Rectangle: width {width} height {height}"));
 
 	if (shape.IsCircle)
-		Console.WriteLine($"This is a circle: {shape.CircleData}");
+		Console.WriteLine($"This is a circle: {shape}");
 }
 
 void WriteUnion(TestUnion testUnion)
@@ -71,4 +70,10 @@ static unsafe ReadOnlySpan<byte> Marshal<T>(T value)
     where T : unmanaged
 {
     return new ReadOnlySpan<byte>(Unsafe.AsPointer(ref value), sizeof(T));
+}
+
+static unsafe void WriteSizeOf<T>()
+	where T : unmanaged
+{
+	Console.WriteLine($"Size of {typeof(T).Name}: {sizeof(T)}");
 }
