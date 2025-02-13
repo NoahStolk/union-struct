0c4c0d29e729bc53fe2c7c9925ef2d89e9d48522
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2025-02-13 21:47:00 +0100

    Add test-pack.bat

---
+++ scripts/test-pack.bat
@@ -0,0 +1,8 @@
+del "%~dp0\..\src\artifacts\NoahStolk.UnionStruct.0.0.0-temp.nupkg"
+cd "..\src"
+dotnet pack -c Release -o ./artifacts -p:Version=0.0.0-temp
+dotnet restore UnionStruct.Tests.NuGetIntegration/UnionStruct.Tests.NuGetIntegration.csproj --packages ./packages --configfile "nuget.integration-tests.config"
+dotnet build UnionStruct.Tests.NuGetIntegration/UnionStruct.Tests.NuGetIntegration.csproj -c Release --packages ./packages --no-restore
+dotnet test UnionStruct.Tests.NuGetIntegration/UnionStruct.Tests.NuGetIntegration.csproj -c Release --no-build --no-restore
+
+pause
